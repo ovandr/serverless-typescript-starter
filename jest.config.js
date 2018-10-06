896a1d0f77c09fe374c6669da2b3f3dcee7b9712
ovandr <24862781+ovandr@users.noreply.github.com>
2018-10-06 13:22:39 +0300

    Simplify ts-jest config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,4 @@
 module.exports = {
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    "moduleFileExtensions": [
-        "ts",
-        "tsx",
-        "js",
-        "jsx",
-        "json",
-        "node"
-    ],
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
